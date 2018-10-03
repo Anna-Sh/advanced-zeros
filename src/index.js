4cd2bafdda9a74788ecbb9f8753a2a424a929932
Anna-Sh <a.shmidova@yandex.ru>
2018-10-03 14:32:47 +0300

    advzeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+function getPrimesList( n ) {
+    const sqrt = Math.sqrt(n);
+    let currentValue = n;
+    let multiplier = 2;
+
+    const result = [];
+
+    while (currentValue > 1 && multiplier <= sqrt) {
+        if (currentValue % multiplier === 0)
+        {
+            result.push(multiplier);
+            currentValue /= multiplier;
+        }
+        else if (multiplier === 2)
+        {
+            multiplier++;
+        }
+        else
+        {
+            multiplier += 2;
+        }
+    }
+
+    if (currentValue !== 1) {
+        result.push(currentValue);
+    }
+
+    return result.reduce( (accumulator, element) => {
+
+        const existing = accumulator.find( ( el ) => el.number === element );
+
+        if (existing) {
+            existing.exp++;
+        } else {
+            accumulator.push({
+                number: element,
+                exp: 1
+            });
+        }
+
+        return accumulator
+
+    }, []);
 }
+
+module.exports = function getZerosCount(number, base) {
+
+    let result = null;
+
+    const basePrimes = getPrimesList(base);
+
+    for (let prime of basePrimes) {
+
+        let k = 0;
+
+        for (let i = prime.number; i <= number; i *= prime.number) {
+            k += Math.floor(number / i);
+        }
+
+        k = Math.floor( k / prime.exp );
+
+        if (!result || k < result) {
+            result = k;
+        }
+
+    }
+    return result;
+};
+
+
+
